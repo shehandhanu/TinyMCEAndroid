dd68a4d4602b0a38bac2bdf991768592aa329dc6
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2022-02-02 12:27:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication123/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication123;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://shehandhanu.github.io/TinyMCE/index");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+    }
+}
